75ff96dce4ab3e28cbfe027d01612117ee5a7042
lomakim <lomakim@gmail.com>
2026-03-08 14:59:53 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 // COMSC-200 | Assignment 3A | Loma Kim
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Marble {
     private:
-    static unsigned int count = 0;
+    static unsigned int count;
     unsigned int number;
     double radius;
 
@@ -16,15 +17,29 @@ class Marble {
     //Mutator functions
     void setRadius(double r) { radius = r; }
 
-    double getVolume() { return (4/3 * 3.14159 * r); }
+    double getVolume() { return (4/3 * 3.14159 * radius * radius * radius); }
+    Marble operator++();
+    Marble operator++(int);
+    Marble operator+(const Marble &);
+    Marble operator-(const Marble &);
 
 };
 
 
 int main() {
+    Marble m1(1.3);
+    cout << "Radius: " << m1.getRadius() << endl;
+    cout << "Volume: " << m1.getVolume() << endl;
+    cout << "Number: " << m1.getNumber() << endl;
+    m1++;
+    cout << "New Radius: " << m1.getRadius() << endl;
+    ++m1;
+    cout << "New Radius: " << m1.getRadius() << endl;
+
     return 0;
 }
 
+unsigned int Marble::count = 0;
 
 /*CONSTRUCTOR - default constructor which can accept an argument:
     - increments count
@@ -33,4 +48,30 @@ int main() {
 Marble::Marble(double r = 0){
     number = ++count;
     radius = r;
+}
+
+//OVERLOADED PREFIX INCREMENT OPERATOR
+Marble Marble::operator++() {
+    ++radius;
+    return *this;
+}
+
+//OVERLOADED POSTFIX INCREMENT OPERATOR
+Marble Marble::operator++(int) {
+    Marble temp(radius);    // create temp Marble to hold radius value
+    count--;                // decrement count value as temp is not a part of the marble count
+    radius++;               // increment radius of this Marble
+    return temp;            // return original value of this Marble
+}
+
+//OVERLOADED ADDITION OPERATOR
+Marble Marble::operator+(const Marble& rhs) {
+    Marble temp;
+    return temp;
+}
+
+//OVERLOADED SUBTRACTION OPERATOR
+Marble Marble::operator-(const Marble& rhs) {
+    Marble temp;
+    return temp;
 }
